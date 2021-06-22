d4d0cbb8d8bdf9f634aaf07b4e48efd6bb2e1c42
Dmitrii <dimzfresh@gmail.com>
2021-06-22 21:16:51 +0300

    add custom fonts and colors

--- CryptoMessenger/CryptoMessenger/StyleGuide/Palette.swift
+++ CryptoMessenger/CryptoMessenger/StyleGuide/Palette.swift
@@ -6,9 +6,10 @@ import SwiftUI
 enum Palette: Hashable {
     case clear,
          black(_ alpha: CGFloat = 1),
+         lightGray(_ alpha: CGFloat = 1),
          gray(_ alpha: CGFloat = 1),
          white(_ alpha: CGFloat = 1),
-         purple(_ alpha: CGFloat = 1)
+         blue(_ alpha: CGFloat = 1)
 
     // MARK: - Internal Properties
 
@@ -17,13 +18,15 @@ enum Palette: Hashable {
         case .clear:
             return .clear
         case let .black(alpha):
-            return .init(r: 61, g: 61, b: 61, a: alpha)
+            return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).withAlphaComponent(alpha)
         case let .gray(alpha):
-            return .init(r: 229, g: 229, b: 229, a: alpha)
+            return #colorLiteral(red: 0.8980392157, green: 0.8980392157, blue: 0.8980392157, alpha: 1).withAlphaComponent(alpha)
+        case let .lightGray(alpha):
+            return #colorLiteral(red: 0.9607843137, green: 0.9647058824, blue: 0.9725490196, alpha: 1).withAlphaComponent(alpha)
         case let .white(alpha):
-            return .init(r: 255, g: 255, b: 255, a: alpha)
-        case let .purple(alpha):
-            return .init(r: 230, g: 150, b: 254, a: alpha)
+            return #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).withAlphaComponent(alpha)
+        case let .blue(alpha):
+            return #colorLiteral(red: 0.2431372549, green: 0.6039215686, blue: 0.8862745098, alpha: 1).withAlphaComponent(alpha)
         }
     }
 
