d27ee4a747e5a112bb93095ae42654d7d2009f49
Dmitrii <dimzfresh@gmail.com>
2021-07-27 18:15:17 +0300

    fix layout and codestyle

--- CryptoMessenger/CryptoMessenger/UI/Scenes/Profile/ProfileView.swift
+++ CryptoMessenger/CryptoMessenger/UI/Scenes/Profile/ProfileView.swift
@@ -7,25 +7,29 @@ final class ProfileView: UIView {
     // MARK: - Internal Properties
 
     var didTap: (() -> Void)?
+
     // MARK: - Private Properties
-    private lazy var TitleLabel = UILabel()
-    private lazy var MobileLabel = UILabel()
-    private lazy var ProfileImage = UIImageView()
-    private lazy var twitterButton = UIButton(type: .custom)
-    private lazy var siteButton = UIButton(type: .custom)
-    private lazy var facebookButton = UIButton(type: .custom)
-    private lazy var instagramButton = UIButton(type: .custom)
+
+    private lazy var titleLabel = UILabel()
+    private lazy var profileImage = UIImageView()
+    private lazy var phoneLabel = UILabel()
+    private lazy var twitterButton = UIButton()
+    private lazy var siteButton = UIButton()
+    private lazy var facebookButton = UIButton()
+    private lazy var instagramButton = UIButton()
+
     // MARK: - Lifecycle
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         background(.white())
         addProfileImage()
         addTitleLabel()
-        //addMobileLabel()
         addTwitterButton()
-        addsiteButton()
-        addfacebookButton()
-        addinstButton()
+        addSiteButton()
+        addFacebookButton()
+        addInstButton()
+        addPhoneLabel()
     }
 
     @available(*, unavailable)
@@ -33,67 +37,44 @@ final class ProfileView: UIView {
         fatalError("not implemented")
     }
 
-    // MARK: - Internal Methods
-
     // MARK: - Private Methods
+
+    private func addProfileImage() {
+        profileImage.snap(parent: self) {
+            $0.image = R.image.profile.userAvatar()
+            $0.clipCorners(radius: 50)
+        } layout: {
+            $0.width.height.equalTo(100)
+            $0.top.equalTo(self.snp_topMargin).offset(24)
+            $0.leading.equalTo($1).offset(16)
+        }
+    }
 
     private func addTitleLabel() {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineHeightMultiple = 1.15
-        TitleLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
-        TitleLabel.numberOfLines = 0
-        TitleLabel.snap(parent: self) {
-                    $0.titleAttributes(
-                        text: "ИКЕА Россия",
-                        [
-                            .paragraph(paragraphStyle),
-                            .font(.medium(15)),
-                            .color(.black())
-                        ]
-                    )
-                    $0.textAlignment = .center
-                    $0.numberOfLines = 0
-                } layout: {
-                    $0.top.equalTo(self.snp_topMargin).offset(27)
-                    $0.leading.equalTo($1).offset(132)
-                }
-            }
-    private func addProfileImage() {
-        ProfileImage.snap(parent: self) {
-            $0.image = R.image.profile.userAvatar()
-                $0.layer.masksToBounds = false
-                $0.contentMode = .scaleAspectFit
-                $0.frame.size = CGSize(width: 100, height: 100) // размеры новой картинки
-                $0.layer.cornerRadius = $0.frame.height / 2
-                $0.clipsToBounds = true
-                } layout: {
-                    $0.top.equalTo(self.snp_topMargin).offset(24)
-                    $0.leading.equalTo($1).offset(16)
-                    $0.trailing.equalTo($1).offset(-259)
-                }
+        paragraphStyle.alignment = .left
 
+        titleLabel.snap(parent: self) {
+            $0.titleAttributes(
+                text: "ИКЕА Россия",
+                [
+                    .paragraph(paragraphStyle),
+                    .font(.medium(15)),
+                    .color(.black())
+                ]
+            )
+            $0.lineBreakMode = .byWordWrapping
+            $0.numberOfLines = 0
+            $0.textAlignment = .left
+        } layout: {
+            $0.top.equalTo(self.snp_topMargin).offset(27)
+            $0.height.equalTo(18)
+            $0.leading.equalTo($1).offset(132)
+            $0.trailing.equalTo($1).offset(-45)
+        }
     }
-    private func addMobileLabel() {
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineHeightMultiple = 17.78
-        paragraphStyle.alignment = .center
-        TitleLabel.snap(parent: self) {
-                    $0.titleAttributes(
-                        text: "8 (925) 851-39-11",
-                        [
-                            .paragraph(paragraphStyle),
-                            .font(.medium(15)),
-                            .color(.black())
-                        ]
-                    )
-                    $0.textAlignment = .center
-                    $0.numberOfLines = 0
-                } layout: {
-                    $0.top.equalTo($1).offset(56)
-                    $0.leading.equalTo(self.ProfileImage.snp.trailing).offset(16)
-                    $0.trailing.equalTo($1).offset(-45)
-                }
-    }
+
     private func addTwitterButton() {
         twitterButton.snap(parent: self) {
             let image = R.image.profile.twitter()
@@ -104,28 +85,20 @@ final class ProfileView: UIView {
             $0.height.width.equalTo(32)
         }
     }
-    private func addsiteButton() {
-        twitterButton.snap(parent: self) {
+
+    private func addSiteButton() {
+        siteButton.snap(parent: self) {
             let image = R.image.profile.website()
             $0.setImage(image, for: .normal)
         } layout: {
             $0.height.width.equalTo(32)
             $0.top.equalTo($1).offset(57)
-            $0.leading.equalTo($1).offset(181)
+            $0.leading.equalTo($1).offset(172)
         }
     }
-    private func addinstButton() {
-        twitterButton.snap(parent: self) {
-            let image = R.image.profile.instagram()
-            $0.setImage(image, for: .normal)
-        } layout: {
-            $0.height.width.equalTo(32)
-            $0.top.equalTo($1).offset(57)
-            $0.leading.equalTo($1).offset(252)
-        }
-    }
-    private func addfacebookButton() {
-        twitterButton.snap(parent: self) {
+
+    private func addFacebookButton() {
+        instagramButton.snap(parent: self) {
             let image = R.image.profile.facebook()
             $0.setImage(image, for: .normal)
         } layout: {
@@ -135,4 +108,36 @@ final class ProfileView: UIView {
         }
     }
 
+    private func addInstButton() {
+        facebookButton.snap(parent: self) {
+            let image = R.image.profile.instagram()
+            $0.setImage(image, for: .normal)
+        } layout: {
+            $0.height.width.equalTo(32)
+            $0.top.equalTo($1).offset(57)
+            $0.leading.equalTo($1).offset(252)
+        }
+    }
+
+    private func addPhoneLabel() {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = 1.15
+        paragraphStyle.alignment = .left
+
+        phoneLabel.snap(parent: self) {
+            $0.titleAttributes(
+                text: "8 (925) 851-39-11",
+                [
+                    .paragraph(paragraphStyle),
+                    .font(.regular(15)),
+                    .color(.black())
+                ]
+            )
+            $0.textAlignment = .left
+        } layout: {
+            $0.top.equalTo(self.snp_topMargin).offset(101)
+            $0.leading.equalTo(self.profileImage.snp.trailing).offset(16)
+            $0.trailing.equalTo($1).offset(-45)
+        }
+    }
 }
