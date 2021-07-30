e35fe1dcd4d8f61223731457712f296d5f12def1
Igor <igor9.i@yandex.ru>
2021-07-30 17:05:21 +0300

    Auto stash before checking out "HEAD"

---
+++ CryptoMessenger/CryptoMessenger/UI/Scenes/Profile/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  CryptoMessenger
+//
+//  Created by Игорь Коноваленко on 30.07.2021.
+//
+
+import Foundation
+import UIKit
+
+struct Profile {
+    var image: UIImage
+}
+
+
