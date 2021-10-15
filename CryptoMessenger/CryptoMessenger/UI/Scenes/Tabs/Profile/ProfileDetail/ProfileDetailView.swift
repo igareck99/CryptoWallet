13cfba6e9bd32e3ead1d3135a83930aeff5b7e3d
Dmitrii <dimzfresh@gmail.com>
2021-10-15 11:27:44 +0300

    final MR fixes

--- CryptoMessenger/CryptoMessenger/UI/Scenes/Tabs/Profile/ProfileDetail/ProfileDetailView.swift
+++ CryptoMessenger/CryptoMessenger/UI/Scenes/Tabs/Profile/ProfileDetail/ProfileDetailView.swift
@@ -112,6 +112,8 @@ final class ProfileDetailView: UIView {
     }
 }
 
+// MARK: - ProfileDetailView (ProfileDetailDelegate)
+
 extension ProfileDetailView: ProfileDetailDelegate {
     func update(_ cell: UITableViewCell, _ textView: UITextView) {
         if let indexPath = tableView.indexPath(for: cell) {
