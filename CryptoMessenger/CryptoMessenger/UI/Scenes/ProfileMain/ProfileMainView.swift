f3d9936425cb9d150fd7b0c0744668a70431ece9
Igor <igor9.i@yandex.ru>
2021-12-01 14:25:46 +0300

    to review

--- CryptoMessenger/CryptoMessenger/UI/Scenes/ProfileMain/ProfileMainView.swift
+++ CryptoMessenger/CryptoMessenger/UI/Scenes/ProfileMain/ProfileMainView.swift
@@ -7,6 +7,7 @@ struct ProfileMainView: View {
     // MARK: - Internal Properties
 
     var profile: ProfileUserItem
+    var gridItems: [GridItem] = [GridItem(), GridItem(), GridItem()]
 
     // MARK: - Body
 
@@ -24,7 +25,6 @@ struct ProfileMainView: View {
                                 Text(profile.name)
                                     .font(.medium(15))
                                 Button(R.string.localizable.profileAdd()) {
-                                    
                                 }.frame(width: 160, height: 32)
                                     .font(.regular(15))
                                     .overlay(
@@ -33,7 +33,7 @@ struct ProfileMainView: View {
                                     )
                                 Text(profile.number)
                             }
-                        }
+                        }.padding(.leading, 16)
                         VStack(alignment: .leading, spacing: 2) {
                             Text(profile.info)
                                 .font(.regular(15))
@@ -41,7 +41,7 @@ struct ProfileMainView: View {
                             Text(R.string.localizable.profileSite())
                                 .font(.regular(15))
                                 .foreground(.blue())
-                        }
+                        }.padding(.leading, 16)
                         VStack(alignment: .center) {
                             Button(R.string.localizable.profileAdd()) {
                             }.frame(width: geometry.size.width
@@ -51,7 +51,18 @@ struct ProfileMainView: View {
                                     RoundedRectangle(cornerRadius: 8)
                                         .stroke(.blue, lineWidth: 1)
                                 )
-                        }
+                        }.padding(.leading, 16)
+                        LazyVGrid(columns: gridItems, alignment: .center, spacing: 1.5) {
+                            ForEach((0...profile.photos.count - 1), id: \.self) { number in
+                                        VStack {
+                                            Image(uiImage: profile.photos[number] ?? UIImage())
+                                                .resizable()
+                                                .frame(width: (geometry.size.width - 3) / 3,
+                                                       height: (geometry.size.width - 3) / 3, alignment: .center)
+                                                .scaledToFit()
+                                        }
+                                    }
+                                }
                     }
                     .navigationBarTitleDisplayMode(.inline)
                     .toolbar {
