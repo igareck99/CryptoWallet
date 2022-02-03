8c3e16bc5fe848bf96e964470c75b61c5823c3e4
Igor <igor9.i@yandex.ru>
2022-02-03 16:20:34 +0300

    delete excess views

--- CryptoMessenger/CryptoMessenger/UI/Scenes/Answers/AnswerView.swift
+++ CryptoMessenger/CryptoMessenger/UI/Scenes/Answers/AnswerView.swift
@@ -21,28 +21,6 @@ struct AnswerCellView: View {
     }
 }
 
-// MARK: - AnswerAdditionalCellView
-
-struct AnswerAdditionalCellView: View {
-
-    // MARK: - Internal Properties
-
-    var text: String
-
-    // MARK: - Body
-
-    var body: some View {
-        HStack(alignment: .center) {
-            Text(text)
-                .font(.regular(16))
-                .frame(minHeight: 24,
-                       maxHeight: 44,
-                       alignment: .leading)
-            Spacer()
-        }
-    }
-}
-
 // MARK: - AnswerView
 
 struct AnswerView: View {
@@ -50,45 +28,50 @@ struct AnswerView: View {
     // MARK: - Internal Properties
 
     @StateObject var viewModel: AnswersViewModel
-    @State private var tappedCell: Int?
 
     // MARK: - Body
 
     var body: some View {
-        VStack(spacing: 16) {
             Divider().padding(.top, 16)
             List {
                 ForEach(0...viewModel.listData.count - 1, id: \.self) { index in
-                    AnswerCellView(item: viewModel.listData[index])
+                    if viewModel.listData[index].tapped {
+                        AnswerCellView(item: viewModel.listData[index])
+                        .background(.white())
+                        .listRowSeparator(.visible, edges: .bottom)
+                        .onTapGesture {
+                            viewModel.listData[index].tapped.toggle()
+                        }
+                    } else {
+                        AnswerCellView(item: viewModel.listData[index])
                         .background(.white())
                         .listRowSeparator(.hidden)
                         .onTapGesture {
                             viewModel.listData[index].tapped.toggle()
                         }
+
+                    }
                     if viewModel.listData[index].tapped {
-                        ForEach(viewModel.listData[index].details) { item in
-                            AnswerAdditionalCellView(text: item.text)
-                                .background(.lightBlue())
-                                .listRowSeparator(.hidden)
+                        withAnimation(.linear(duration: 0.15)) {
+                            ForEach(viewModel.listData[index].details) { item in
+                                Text(item.text)
+                                    .font(.regular(16))
+                                    .frame(minHeight: 24,
+                                           maxHeight: 44,
+                                           alignment: .leading)
+                                    .listRowSeparator(.hidden)
+                            }
+                            .listRowBackground(Color(.lightBlue()))
                         }
                     }
                 }
-            }
-            .listStyle(.inset)
         }
+            .listStyle(.plain)
         .toolbar {
             ToolbarItem(placement: .principal) {
                 Text(R.string.localizable.additionalMenuQuestions())
                     .font(.bold(15))
             }
         }
-    }
-}
-
-// MARK: - SessionListView_Previews
-
-struct AV_Previews: PreviewProvider {
-    static var previews: some View {
-        AnswerAdditionalCellView(text: R.string.localizable.answersNotesShare())
     }
 }
