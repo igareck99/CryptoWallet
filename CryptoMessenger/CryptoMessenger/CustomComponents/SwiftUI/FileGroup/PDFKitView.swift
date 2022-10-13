f2dd8e79b4197daaadaca856f5b30de2f52d4a12
Igareck99 <igor9.i@yandex.ru>
2022-10-13 21:44:26 +0300

    fix fileView update in chat historyView

--- CryptoMessenger/CryptoMessenger/CustomComponents/SwiftUI/FileGroup/PDFKitView.swift
+++ CryptoMessenger/CryptoMessenger/CustomComponents/SwiftUI/FileGroup/PDFKitView.swift
@@ -23,23 +23,28 @@ struct PDFKitRepresentedView: UIViewRepresentable {
     // MARK: - Internal Properties
 
     let url: URL
+    var viewModel: PDFKitViewModel
 
     // MARK: - Life Cycle
 
     init(_ url: URL) {
         self.url = url
+        self.viewModel = PDFKitViewModel(url: url)
     }
 
     // MARK: - Internal Methods
 
     func makeUIView(context: UIViewRepresentableContext<PDFKitRepresentedView>) -> PDFKitRepresentedView.UIViewType {
         let pdfView = PDFView()
-        DispatchQueue.global(qos: .background).async {
-            pdfView.document = PDFDocument(url: url)
-        }
+        pdfView.document = PDFDocument(data: viewModel.data)
         pdfView.autoScales = true
         return pdfView
     }
 
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PDFKitRepresentedView>) {}
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PDFKitRepresentedView>) {
+        viewModel.downloadPdf { data in
+            guard let data = data else { return }
+            viewModel.data = data
+        }
+    }
 }
