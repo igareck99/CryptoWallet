e77513cf956a83b8666f7e7d3646aa9792994adf
Dmitry4Alexandrov <justv4lera@yandex.ru>
2023-06-01 11:31:49 +0300

    appereance meneger

--- CryptoMessenger/CryptoMessenger/UI/Scenes/Tabs/Profile/Initial/Components/ProfileSettings/ProfileSettingsMenuView.swift
+++ CryptoMessenger/CryptoMessenger/UI/Scenes/Tabs/Profile/Initial/Components/ProfileSettings/ProfileSettingsMenuView.swift
@@ -3,15 +3,33 @@ import SwiftUI
 // MARK: - ProfileSettingsMenuView
 
 struct ProfileSettingsMenuView: View {
-
+    class Utilities {
+        
+        // MARK: - Internal Properties
+        @AppStorage("selectedAppearance") var selectedAppearance = 0
+        var userInterfaceStyle: ColorScheme? = .dark
+        
+        func overrideDisplayMode() {
+            var userInterfaceStyle: UIUserInterfaceStyle
+            
+            if selectedAppearance == 2 {
+                userInterfaceStyle = .dark
+            } else if selectedAppearance == 1 {
+                userInterfaceStyle = .light
+            } else {
+                userInterfaceStyle = .unspecified
+            }
+            UIApplication.shared.windows.first?.overrideUserInterfaceStyle = userInterfaceStyle
+        }
+    }
     // MARK: - Internal Properties
-
+    @AppStorage("selectedAppearance") var selectedAppearance = 0
     @StateObject var viewModel = ProfileSettingsMenuViewModel()
     let balance: String
     let onSelect: GenericBlock<ProfileSettingsMenu>
-
+    var utilities = Utilities()
     // MARK: - Body
-
+    
     var body: some View {
         VStack(spacing: 0) {
             RoundedRectangle(cornerRadius: 2)
@@ -31,13 +49,13 @@ struct ProfileSettingsMenuView: View {
             }
             .padding(.top, 8)
             .padding([.leading, .trailing], 16)
-
+            
             Divider()
                 .foreground(.grayE6EAED())
                 .padding(.top, 16)
-
+            
             List {
-				ForEach(viewModel.settingsTypes(), id: \.self) { type in
+                ForEach(viewModel.settingsTypes(), id: \.self) { type in
                     if type == .questions {
                         Divider()
                             .listRowInsets(.init())
@@ -57,6 +75,23 @@ struct ProfileSettingsMenuView: View {
                 }
             }
             .listStyle(.plain)
-        }
+            HStack {
+                Spacer()
+                Button(action: {selectedAppearance = 1}){
+                    Text("Light")}
+                Spacer()
+                Button(action: {selectedAppearance = 2}){
+                    Text("Dark")}
+                Spacer()
+                Button(action: {selectedAppearance = 0}){
+                    Text("System")}
+                Spacer()
+            }
+            .padding(.bottom, 200)
+            .onChange(of: selectedAppearance, perform: { value in
+                utilities.overrideDisplayMode()
+            })
+            Divider()
+        }.background(selectedAppearance == 1 ? Color.white : selectedAppearance == 2 ? Color.black : Color(UIColor.systemBackground))
     }
 }
