932a6d1f275257ee7a757d6ef09402858d37c353
Timersha <>
2023-06-06 16:14:20 +0300

    color-fix

--- CryptoMessenger/CryptoMessenger/UI/Scenes/Tabs/Chat/ChatRoom/Components/Reactions/Reaction/Message Reactions /ReactionsPalette.swift
+++ CryptoMessenger/CryptoMessenger/UI/Scenes/Tabs/Chat/ChatRoom/Components/Reactions/Reaction/Message Reactions /ReactionsPalette.swift
@@ -55,6 +55,8 @@ extension Color {
     static let vampireBlack = Color(uiColor: .vampireBlack)
     static let policeBlue = Color(uiColor: .policeBlue)
     // MARK: - Old ones
+    static let blackSqueezeApprox = Color(uiColor: .blackSqueezeApprox)
+    static let dodgerBlueApprox = Color(uiColor: .dodgerBlueApprox)
     static let onahauApprox = Color(uiColor: .onahauApprox)
     static let zeroColor = Color(uiColor: .zeroColor)
     static let sailApprox = Color(uiColor: .sailApprox)
@@ -68,8 +70,16 @@ extension Color {
     static let manateeApprox = Color(uiColor: .manateeApprox)
     static let sharkApprox = Color(uiColor: .sharkApprox)
     static let athensGrayApprox = Color(uiColor: .athensGrayApprox)
+    static let ironApprox = Color(uiColor: .ironApprox)
+    static let regentGrayApprox = Color(uiColor: .regentGrayApprox)
+    static let woodSmokeApprox = Color(uiColor: .woodSmokeApprox)
     static let jaffaApprox = Color(uiColor: .jaffaApprox)
+    static let amaranthApprox = Color(uiColor: .amaranthApprox)
+    static let bombayApprox = Color(uiColor: .bombayApprox)
+    static let blackHazeApprox = Color(uiColor: .blackHazeApprox)
     static let white06 = Color(uiColor: .white06)
+    static let polarApprox = Color(uiColor: .polarApprox)
+    static let jungleGreenApprox = Color(uiColor: .jungleGreenApprox)
 }
 
 extension UIColor {
@@ -187,6 +197,8 @@ extension UIColor {
     
     
     // MARK: - Old ones
+    static let blackSqueezeApprox = UIColor(red: 237.0 / 255.0, green: 243.0 / 255.0, blue: 248.0 / 255.0, alpha: 1.0)
+    static let dodgerBlueApprox = UIColor(red: 63.0 / 255.0, green: 170.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
     static let onahauApprox = UIColor(red: 205.0 / 255.0, green: 232.0 / 255.0, blue: 254.0 / 255.0, alpha: 1.0)
     static let zeroColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
     static let sailApprox = UIColor(red: 173.0 / 255.0, green: 214.0 / 255.0, blue: 248.0 / 255.0, alpha: 1.0)
@@ -200,6 +212,14 @@ extension UIColor {
     static let manateeApprox = UIColor(red: 0.557, green: 0.557, blue: 0.576, alpha: 1)
     static let sharkApprox = UIColor(red: 0.11, green: 0.11, blue: 0.118, alpha: 1)
     static let athensGrayApprox = UIColor(red: 0.902, green: 0.918, blue: 0.929, alpha: 1)
+    static let ironApprox = UIColor(red: 220.0 / 255.0, green: 223.0 / 255.0, blue: 226.0 / 255.0, alpha: 1.0)
+    static let regentGrayApprox = UIColor(red: 138.0 / 255.0, green: 145.0 / 255.0, blue: 156.0 / 255.0, alpha: 1.0)
+    static let woodSmokeApprox = UIColor(red: 18.0 / 255.0, green: 19.0 / 255.0, blue: 21.0 / 255.0, alpha: 1.0)
     static let jaffaApprox = UIColor(red: 0.925, green: 0.608, blue: 0.235, alpha: 1)
+    static let amaranthApprox = UIColor(red: 232.0 / 255.0, green: 30.0 / 255.0, blue: 78.0 / 255.0, alpha: 1.0)
+    static let bombayApprox = UIColor(red: 179.0 / 255.0, green: 184.0 / 255.0, blue: 191.0 / 255.0, alpha: 1.0)
+    static let blackHazeApprox = UIColor(red: 248.0 / 255.0, green: 249.0 / 255.0, blue: 249.0 / 255.0, alpha: 1.0)
     static let white06 = UIColor(red: 1, green: 1, blue: 1, alpha: 0.6)
+    static let polarApprox = UIColor(red: 242.0 / 255.0, green: 247.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
+    static let jungleGreenApprox = UIColor(red: 39.0 / 255.0, green: 174.0 / 255.0, blue: 96.0 / 255.0, alpha: 1.0)
 }
