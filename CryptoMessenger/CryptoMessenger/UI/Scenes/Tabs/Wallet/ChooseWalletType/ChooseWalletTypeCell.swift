977fc063d97667592081757e5d71003226f15e99
Dmitry4Alexandrov <justv4lera@yandex.ru>
2023-07-20 11:01:57 +0300

    AURA-363: choose wallet pallete changes

--- CryptoMessenger/CryptoMessenger/UI/Scenes/Tabs/Wallet/ChooseWalletType/ChooseWalletTypeCell.swift
+++ CryptoMessenger/CryptoMessenger/UI/Scenes/Tabs/Wallet/ChooseWalletType/ChooseWalletTypeCell.swift
@@ -19,7 +19,7 @@ struct ChooseWalletTypeCell: View {
                 .padding(.trailing, 16)
             Spacer()
             Divider()
-                .foregroundColor(.ironApprox)
+                .foregroundColor(.chineseBlack)
                 .frame(height: 0.5)
         }
         .padding(.leading, 16)
