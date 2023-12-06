fc907330e5ed32822c16306daa3c05ff13354e8c
Timersha <>
2023-12-06 19:43:09 +0300

    AURA-1023: Wallet fix

--- CryptoMessenger/CryptoMessenger/Extensions/Array+Extensions.swift
+++ CryptoMessenger/CryptoMessenger/Extensions/Array+Extensions.swift
@@ -3,9 +3,9 @@ import Foundation
 // MARK: - Array (Equatable)
 
 extension Array where Element: Equatable {
-
+    
     // MARK: - Internal Properties
-
+    
     func next(item: Element) -> Element? {
         if let index = firstIndex(of: item), index + 1 <= count {
             return index + 1 == count ? self[0] : self[index + 1]
@@ -21,12 +21,18 @@ extension Array where Element: Equatable {
         }
         return uniqueValues
     }
-
+    
     func previous(item: Element) -> Element? {
         if let index = firstIndex(of: item), index >= 0 {
             return index == 0 ? last : self[index - 1]
         }
         return nil
+    }
+}
+
+extension Array where Element: Equatable & Hashable {
+    var asSet: Set<Element> {
+        Set(self)
     }
 }
 
